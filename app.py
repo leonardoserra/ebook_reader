55870bfc50f6b4144bf5187a72754809d98a286a
leonardoserra <serra.leonardo92@gmail.com>
2026-01-10 23:17:57 +0100

    added some type annotations

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+import json
+import random
+
+from typing import TypedDict
+
 from flask import Flask
 
 from flask import render_template
@@ -7,16 +12,24 @@ from flask import url_for
 from flask import flash
 from flask import abort
 
-import json
-
-import random
-
 import ebook_manager
 
+######   data struct   ######
+type CurrentPage = int
+type ScrollY = int
+
+
+class EbookState(TypedDict):
+    current_page: CurrentPage
+    scroll_y: ScrollY
+
+
+######   app   ######
 app = Flask(__name__)
 app.secret_key = random.randbytes(8)
 
 
+#######   routes   #########
 @app.route("/")
 def index():
     ebooks = ebook_manager.choose_book()
@@ -32,14 +45,15 @@ def index():
 
 
 @app.route("/read/<ebook_name>/<int:page_index>")
-def read_ebook(ebook_name="book.epub", page_index=0):
+def read_ebook(ebook_name: str = "book.epub", page_index: int = 0):
     try:
         page_content, page_count = ebook_manager.extract_page(ebook_name, page_index)
-        scroll_y = request.args.get('scroll_y', 0)
-        ebook_manager.set_state(ebook_name, page_index, scroll_y=scroll_y)
+        scroll_y = request.args.get("scroll_y", 0)
+        ebook_manager.set_state(ebook_name, page_index, scroll_y=int(scroll_y))
 
     except Exception as e:
-        abort(e)
+        print(e)
+        abort(1)
 
     ctx = {
         "origin": request.path,
@@ -71,35 +85,37 @@ def load_json():
 
 
 @app.route("/save-state")
-def save_state():
+def save_state() -> str:
     state = request.form.get("state")
     if state:
         with open("./static/state.json", "w") as file:
             file.write(json.dumps(state))
 
+    return ""
+
 
 @app.route("/set-y-axis", methods=["POST"])
 def set_y_axis():
     """
-    api to expose a way to set the y scroll position  
+    api to expose a way to set the y scroll position
     """
-    ebook_name = request.json.get('ebookName', '')
-    scroll_y = request.json.get('scrollY', 0)
+    ebook_name = request.json.get("ebookName", "")
+    scroll_y = request.json.get("scrollY", 0)
 
     ebook_manager.set_state(ebook_name, scroll_y=scroll_y)
-    r = json.dumps({'status': 201})
+    r = json.dumps({"status": 201})
     return r
 
 
 @app.route("/get-y-axis/<ebook_name>", methods=["GET"])
-def get_y_axis(ebook_name):
+def get_y_axis(ebook_name: str):
     """
     api to expose a way to get the y scroll position of a given book
     """
-    state = ebook_manager.load_state(ebook_name)
+
+    state: EbookState = ebook_manager.load_state(ebook_name)
     r = json.dumps({**state})
     return r
-
 
 
 @app.errorhandler(500)
